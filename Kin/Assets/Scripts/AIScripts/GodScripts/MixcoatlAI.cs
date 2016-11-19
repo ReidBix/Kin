180195cc1f5d58053dd46bb3a12541167dd07d58
Soupdoop <jherd23@gmail.com>
2016-11-19 15:21:30 -0500

    Change is Bad

--- Kin/Assets/Scripts/AIScripts/GodScripts/MixcoatlAI.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/MixcoatlAI.cs
@@ -13,16 +13,38 @@ public class MixcoatlAI : BaseGodAI {
 	}
 
 	AIStates currState;
+	public float introDelay;
+	float introCooldown;
+	public int spears;
 
 	// Use this for initialization
 	protected override void Start () {
 		currState = AIStates.Intro;
+		introCooldown = introDelay;
 	}
 	
 	// Update is called once per frame
 	protected override void Update () {
 		switch (currState) {
 		case AIStates.Intro:
+			introCooldown -= Time.deltaTime;
+			if (introCooldown <= 0) {
+				currState = AIStates.SpearShooting;
+			}
+			break;
+		case AIStates.SpearShooting:
+			//IF READY TO SHOOT SPEARS
+			//MAKE SPEAR PROJECTILE + SHOOT IT
+			//spears--
+			if (spears <= 0) {
+				currState = AIStates.SpearGathering;
+			} else {
+				//MOVE TO NEW LOCATION
+			}
+			break;
+		case AIStates.SpearGathering:
+			break;
+		case AIStates.BowStage:
 			break;
 		default:
 			break;
