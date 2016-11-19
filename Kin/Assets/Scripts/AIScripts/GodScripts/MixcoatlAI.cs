a3c54c8e31865969e552c2c1eae788aed9838e96
Soupdoop <jherd23@gmail.com>
2016-11-19 15:05:13 -0500

    Some Changeroos

--- Kin/Assets/Scripts/AIScripts/GodScripts/MixcoatlAI.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/MixcoatlAI.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MixcoatlAI : MonoBehaviour {
+public class MixcoatlAI : BaseGodAI {
+
+	protected enum AIStates
+	{
+		Intro,
+		SpearShooting,
+		SpearGathering,
+		BowStage,
+		AnimalStage
+	}
+
+	AIStates currState;
 
 	// Use this for initialization
-	void Start () {
-	
+	protected override void Start () {
+		currState = AIStates.Intro;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	protected override void Update () {
+		switch (currState) {
+		case AIStates.Intro:
+			break;
+		default:
+			break;
+		}
 	}
 }
